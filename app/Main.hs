1c4e3d8d11de460b0cd92128a3006820bbd6b74c
mangaohua <mangaohua@qq.com>
2016-11-18 00:24:34 +0800

    implement function

--- app/Main.hs
+++ app/Main.hs
@@ -7,24 +7,34 @@ import Network.Wreq
 import Control.Lens
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.UTF8 as U8
+import Control.Monad (replicateM_)
 
 import System.IO
-
-setCookie :: S.ByteString
-setCookie = "device_id=pyiIDQya"
-
-opts = defaults & header "Cookie" .~ [setCookie]
-
-code = "lu84"
-resp = getWith opts $ "http://bus.kuaizhan.com/bus/1.0/apps/55d45b2dde0f01bf5ba98dcf/env/pro/funcs/sohu_multivote?site_id=3755165334&data=58213bccbeacc01b73dfa6a2&uid=weNpJVmk&code=" ++ code
-
-
+import Control.Concurrent(threadDelay)
 
 main :: IO ()
 main = do
-  r<- get "http://bus.kuaizhan.com/auth/api/sms/picvcode.jpg?1479376496872"
-  let device_id = r ^. responseCookie "device_id" . cookieValue
-      content = r ^. responseBody
-  L.writeFile (show device_id) content
+  f <- openFile "/Users/mgh/imgs/codes.txt" WriteMode
+  replicateM_ 10 $ do
+    r<- get "http://bus.kuaizhan.com/auth/api/sms/picvcode.jpg?1479376496872"
+    let device_id = r ^. responseCookie "device_id" . cookieValue
+        content = r ^. responseBody
+    L.writeFile (U8.toString (S.concat ["/Users/mgh/imgs/",device_id,".jpg"])) content
+    hPutStrLn f (U8.toString (S.concat [device_id," "]))
+  hClose f
 
+sendRequest :: IO ()
+sendRequest = do 
+    f <- readFile "/Users/mgh/imgs/codes.txt"
+    mapM_ sendOne $ toTuple f    
+    where
+      toTuple x = map (\l -> (l!!0,l!!1))  $ map words $ lines x
+      sendOne (deviceId,code) = do
+        let opts = defaults & header "Cookie" .~ [U8.fromString $ "device_id=" ++ deviceId]
+        resp <- getWith opts $ "http://bus.kuaizhan.com/bus/1.0/apps/55d45b2dde0f01bf5ba98dcf/env/pro/funcs/sohu_multivote?site_id=3755165334&data=58213bccbeacc01b73dfa6a2&uid=weNpJVmk&code=" ++ code
+        putStrLn $ show $ resp ^. responseBody
+        threadDelay 200
+
+s = mapM_ (\_->sendRequest) [1..30]
 
